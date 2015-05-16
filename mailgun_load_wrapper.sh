2f201783a02ef57a48f7726495e331d7a29f65bb
Kevin Chiou <kevinchiou@KChious-MacBook-Pro.local>
2015-05-15 23:35:26 -0700

    Adjust to milli

--- mailgun_load_wrapper.sh
+++ mailgun_load_wrapper.sh
@@ -13,13 +13,15 @@ start15=$start
 while [ $start15 -lt $end ]
 do	
 
-	starttime=$(date -d @"$start15")
 	end15=$[$start15+900000]
+	start15_milli=$[$start15/1000]
+	echo $start15_milli
+	starttime=$(date -d @"$start15_milli")
 	endtime=$(date -d @"$end15")
 
 	# Load for that segment
 	echo "Loading from "$starttime" to "$endtime"."
-	$HOME/mailgun_load/./mailgun_load.sh $start15 $end15
+	# $HOME/mailgun_load/./mailgun_load.sh $start15 $end15
 
 	# Increment for the next piece
 	start15=$end15
