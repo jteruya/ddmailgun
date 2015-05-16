904fe74ef0044a3b650e4e36bfc119b6e14face6
Kevin Chiou <kevinchiou@KChious-MacBook-Pro.local>
2015-05-15 23:39:37 -0700

    Adjust to milli

--- mailgun_load_incremental.sh
+++ mailgun_load_incremental.sh
@@ -4,7 +4,7 @@
 max_epoch=$(psql -h 10.223.176.157 -p 5432 -A -t -U kchiou -c "SELECT MAX(EventTimestamp) FROM mailgun.events" dev)
 
 # Identify the current timestamp and convert to epoch
-curr_epoch=$(date +"%s")
+curr_epoch=$(date +"%s%3N")
 
 # Perform the mailgun load for the date range identified
 $HOME/mailgun_load/./mailgun_load.sh $max_epoch $curr_epoch
