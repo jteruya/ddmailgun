2e7cde1d4506f5282d43079e32db61fe2470ae50
jonathan <jonathan@doubledutch.me>
2015-09-22 00:03:21 +0000

    modified:   mailgun_load_incremental.sh

--- mailgun_load_incremental.sh
+++ mailgun_load_incremental.sh
@@ -14,3 +14,6 @@ curr_epoch=$(date +"%s%3N")
 # Perform the mailgun load for the date range identified
 #$HOME/mailgun_load/./mailgun_load.sh $max_epoch $curr_epoch
 $HOME/mailgun_load/./mailgun_load_wrapper.sh $max_epoch $curr_epoch
+
+# Insert and update data into mailguncube 
+psql -h 10.223.192.6 -A -p 5432 -U etl -f "sql/mailguncube_etl.sql" analytics
